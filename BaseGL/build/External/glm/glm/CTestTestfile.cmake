c1351a7034bb0cc4595e786a7de327bb4eefea1a
clandrin <clement.landrin@telecom-paristech.fr>
2019-07-31 22:57:24 +0200

    Cleaned build folder.

--- BaseGL/build/External/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cal/homes/clandrin/igr202/BaseGL/External/glm/glm
-# Build directory: /cal/homes/clandrin/igr202/BaseGL/build/External/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- BaseGL/build/External/glm/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cal/homes/clandrin/igr202/BaseGL/External/glm/test
-# Build directory: /cal/homes/clandrin/igr202/BaseGL/build/External/glm/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
